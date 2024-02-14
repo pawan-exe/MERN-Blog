952828a1ccda8254fc2276312781b544f2ce7c38
pawan-exe <pawangupta63431@gmail.com>
2024-02-14 18:59:24 +0530

    Add functionality to the sign up page

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRouter.js";
 import authRoutes from "./routes/authRouter.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
