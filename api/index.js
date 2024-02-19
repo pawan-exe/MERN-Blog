0b70bc168581bc6e45bad12e0cdcb04f9a19c4ba
pawan-exe <pawangupta63431@gmail.com>
2024-02-19 16:58:53 +0530

    add update user api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRouter.js";
 import authRoutes from "./routes/authRouter.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
