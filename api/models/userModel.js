4ac4c4c9010ceacf59948af28fabe899f9f636b7
pawan-exe <pawangupta63431@gmail.com>
2024-02-22 10:03:38 +0530

    add admin functionality to the user

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema(
       default:
         "https://static.vecteezy.com/system/resources/previews/020/765/399/non_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg",
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
