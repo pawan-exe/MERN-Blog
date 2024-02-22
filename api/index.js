df398bfbcc3b82650309b0ccb0a7744be7ad040b
pawan-exe <pawangupta63431@gmail.com>
2024-02-22 11:55:56 +0530

    Add create post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRouter.js";
 import authRoutes from "./routes/authRouter.js";
+import postRoutes from "./routes/postRouter.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -30,6 +31,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
