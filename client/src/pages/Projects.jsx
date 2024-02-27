2788b2360106d76aae44279c23c5698e1bd55587
pawan-exe <pawangupta63431@gmail.com>
2024-02-27 13:46:12 +0530

    Complete About and Projects page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import CallToAction from "../components/CallToAction";
 
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-3xl mx-auto flex flex-col  mt-28 p-3 items-center gap-6">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Take a look at all the cool stuff I've made!
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
