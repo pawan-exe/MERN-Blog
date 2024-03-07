47951da651a414446362ceef6dbb9c140affbf65
pawan-exe <pawangupta63431@gmail.com>
2024-03-07 13:35:24 +0530

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import postRoutes from "./routes/postRouter.js";
 import commentRoutes from "./routes/commentRouter.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -19,6 +20,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -34,6 +37,12 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
